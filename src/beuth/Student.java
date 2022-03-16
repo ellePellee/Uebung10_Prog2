209db31f580f5669ac7db48818bfd92a9450a97f
ElenaWitecka <elenawitecka@gmx.de>
2022-03-16 13:35:20 +0100

    hopefully can uploead the two vlasses Student and FalscherStudiengangAusnahme

--- src/beuth/Student.java
+++ src/beuth/Student.java
@@ -13,20 +13,20 @@ public class Student {
 	private int matrikelnummer = 0;
 	private String studiengang = "";
 	private Map<String, String> faecherZuNoten = null;
-	
+
 	//-------------------------------------------
 	// Konstruktor
 	public Student(String datenZeile) throws FalscherStudiengangAusnahme {
-		
+
 		try {
 			// Daten in Teile aufteilen
 			String[] teile = datenZeile.split(",");
-			
+
 			// Die Teile in Attribute zuordnen bzw. bearbeiten
 			name = teile[0];
 			matrikelnummer = Integer.parseInt(teile[1]);
 			studiengang = teile[2];
-			
+
 			// Wenn noetig, Ausnahme werfen
 			if (!studiengang.equalsIgnoreCase("Medieninformatik") &&
 					!studiengang.equalsIgnoreCase("Technische Informatik") &&
@@ -35,7 +35,7 @@ public class Student {
 				FalscherStudiengangAusnahme e = new FalscherStudiengangAusnahme(studiengang + " gehoert nicht zum Fachbereich VI");
 				throw e;
 			}
-			
+
 			faecherZuNoten = new HashMap<String, String>();
 		}
 		catch (ArrayIndexOutOfBoundsException e) {
@@ -45,7 +45,7 @@ public class Student {
 			throw e;
 		}
 	}
-	
+
 	//-------------------------------------------
 	// Methoden
 	public int getMatrikelnummer() {
@@ -55,15 +55,15 @@ public class Student {
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getStudiengang() {
 		return studiengang;
 	}
-	
+
 	public void noteEintragen(String fach, String note) {
 		faecherZuNoten.put(fach, note);
 	}
-	
+
 	public void setMatrikelnummer(int matrikelnummer) {
 		this.matrikelnummer = matrikelnummer;
 	}
@@ -82,25 +82,25 @@ public class Student {
 				+ studiengang;
 		return returnString;
 	}
-	
+
 	public List<String> zeugnisZeilenAbrufen() {
 		List<String> returnList = new ArrayList<String>();
 		returnList.add(toString());
-		
-		// Faecher alphabetisch anordnen 
+
+		// Faecher alphabetisch anordnen
 		List<String> faecher = new ArrayList<String>(faecherZuNoten.keySet());
 		Collections.sort(faecher);
-		
+
 		// Eine Zeile fuer jedes Fach erstellen:
 		// Name des Faches <tab> Note
 		for (String fach : faecher) {
 			returnList.add(fach + "\t" + faecherZuNoten.get(fach));
 		}
-		
+
 		return returnList;
 	}
 
 	public Map<String, String> getFaecherZuNoten() {
 		return faecherZuNoten;
-	}	
+	}
 }
